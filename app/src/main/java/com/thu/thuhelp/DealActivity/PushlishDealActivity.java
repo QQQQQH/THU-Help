3461b69d9d49eccb8a76b837586e7ec4b413c034
Mituian <NatumeMituan@gmail.com>
2020-06-13 16:57:22 +0800

    添加任务发布页面

---
+++ app/src/main/java/com/thu/thuhelp/DealActivity/PushlishDealActivity.java
@@ -0,0 +1,14 @@
+package com.thu.thuhelp.DealActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PushlishDealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pushlish_deal);
+    }
+}
