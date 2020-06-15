d7cca3d0f63b38b7dfac21118f67e3f9f9c830a2
myosotisx <sthuiyi6@163.com>
2020-06-15 18:01:51 +0800

    Update MyFragment.java

--- app/src/main/java/com/thu/thuhelp/MainActivity/MyFragment.java
+++ app/src/main/java/com/thu/thuhelp/MainActivity/MyFragment.java
@@ -98,10 +98,16 @@ public class MyFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        setLoginLayout();
+        setUserInfo();
     }
 
-    private void setLoginLayout() {
+    private void setLoginView(String nickname, String balance) {
+        view.findViewById(R.id.financeLayout).setVisibility(View.VISIBLE);
+        view.findViewById(R.id.dealLayout).setVisibility(View.VISIBLE);
+
+    }
+
+    private void setUserInfo() {
         String skey = app.getSkey();
         if (skey == null) {
             return;
@@ -122,8 +128,6 @@ public class MyFragment extends Fragment {
                     JSONObject jsonObject = new JSONObject(resStr);
                     int statusCode = jsonObject.getInt("status");
                     if (statusCode == 200) {
-//                        ((TextView) view.findViewById(R.id.nickname_label)).setText(jsonObject.getString("nickname"));
-//                        ((TextView) view.findViewById(R.id.balance_label)).setText(jsonObject.getString("balance"));
                     }
                     else {
                         activity.runOnUiThread(() -> Toast.makeText(activity, resStr, Toast.LENGTH_LONG).show());
@@ -133,7 +137,5 @@ public class MyFragment extends Fragment {
                 }
             }
         });
-        view.findViewById(R.id.financeLayout).setVisibility(View.VISIBLE);
-        view.findViewById(R.id.dealLayout).setVisibility(View.VISIBLE);
     }
 }
